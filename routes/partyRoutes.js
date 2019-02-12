469b39c3a255d59d36006c4602c1a661a7892da6
Niyitangasam <niyitangasam@gmail.com>
2019-02-12 11:27:28 +0200

    [Feature

--- routes/partyRoutes.js
+++ routes/partyRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/', partyController.createParty);
 router.get('/', partyController.getAllParty);
 router.get('/:id', partyController.getOnlyOne);
-router.patch('/:id/:name', partyController.editPartyName);
+router.patch('/:id/name', partyController.editPartyName);
 router.delete('/:id', partyController.deleteParty);
 
 
