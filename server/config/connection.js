eebd8eb9c7455af109274ccf00db3477a452c636
Niyitangasam <niyitangasam@gmail.com>
2019-02-17 14:42:20 +0200

    [chore 164030986] added tests and authorizations in test

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,6 +6,6 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DB_URL,
 });
-pool.on('connect', () => console.log('DATABASE CONNECTED'));
+// pool.on('connect', () => console.log('DATABASE CONNECTED'));
 
 export default pool;
