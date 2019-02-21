40aa9b3ccd69c178b7ff9c051e0eedf746826f52
Niyitangasam <niyitangasam@gmail.com>
2019-02-22 00:31:41 +0200

    Add a seeder file

---
+++ server/model/seeder.sql
@@ -0,0 +1,24 @@
+ -- offices
+INSERT INTO offices(type, name) VALUES("Legislative", "Rwama");
+INSERT INTO offices(type, name) VALUES("Federal", "Kiro");
+INSERT INTO offices(type, name) VALUES("State", "kibutye");
+INSERT INTO offices(type, name) VALUES("Legislative", "Kigali");
+INSERT INTO offices(type, name) VALUES("Legislative", "Ruhango");
+
+
+-- Parties
+
+INSERT INTO parties(name, hqAddress, logoUrl) VALUES("KMNR", "Butare", "https://wwww/vkbm.v")
+INSERT INTO parties(name, hqAddress, logoUrl) VALUES("PMTR", "Kigali", "https://wwww/vkkm.v")
+INSERT INTO parties(name, hqAddress, logoUrl) VALUES("TRMB", "Kigali", "https://wwww/vhnm.v")
+INSERT INTO parties(name, hqAddress, logoUrl) VALUES("RFGB", "Butare", "https://wwww/vkmm.v")
+INSERT INTO parties(name, hqAddress, logoUrl) VALUES("YGTH", "Butare", "https://wwww/vkmm.v");
+
+
+
+
+
+
+
+
+
